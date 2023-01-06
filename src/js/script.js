7446e37108bbcc623a24c79596154434d106514e
Ulyanina <us19_3_9@yahoo.com>
2023-01-06 14:26:12 +0100

    myPortfolio finisced and responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,26 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const menu = document.querySelector('.menu'),
-//     menuItem = document.querySelectorAll('.menu_item'),
-//     hamburger = document.querySelector('.hamburger');
+//menu
+window.addEventListener('DOMContentLoaded', () => {
+	const hamburger = document.querySelector('.hamburger'),
+		menu = document.querySelector('.menu'),
+		menuItem = document.querySelectorAll('.menu__links-link'),
+		veil = document.querySelector('.veil'),
+		closeBtn = document.querySelector('.menu__close');
 
-//     hamburger.addEventListener('click', () => {
-//         hamburger.classList.toggle('hamburger_active');
-//         menu.classList.toggle('menu_active');
-//     });
-
-//     menuItem.forEach(item => {
-//         item.addEventListener('click', () => {
-//             hamburger.classList.toggle('hamburger_active');
-//             menu.classList.toggle('menu_active');
-//         })
-//     })
-// })
+	hamburger.addEventListener('click', () => {
+		menu.classList.toggle('menu-active');
+		veil.classList.toggle('presentation-veil');
+	});
+	closeBtn.addEventListener('click', () => {
+		menu.classList.toggle('menu-active');
+		veil.classList.toggle('presentation-veil');
+	});
+	menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+			menu.classList.toggle('menu-active');
+			veil.classList.toggle('presentation-veil');
+      });
+   });
+})
 
 //validation
 function validateForms(form) {
@@ -54,3 +60,18 @@ function validateForms(form) {
 	});
 }
 validateForms('#contacts__forms-row');
+
+//Smooth scroll and pageup
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1600) {
+		$('.pageup').fadeIn();
+	} else {
+		$('.pageup').fadeOut();
+	}
+});
+
+$("a[href='#up']").click(function () {
+	const _href = $(this).attr('href');
+	$('html, body').animate({ scrollTop: $(_href).offset().up + 'px' });
+	return false;
+});
