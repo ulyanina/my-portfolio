f64a1371d0a6cbf689e244ed901f1c07db91c4e3
Ulyanina <us19_3_9@yahoo.com>
2023-01-07 16:12:08 +0100

    Changed presentation section, added portfolio content

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,51 @@
-//menu
-window.addEventListener('DOMContentLoaded', () => {
-	const hamburger = document.querySelector('.hamburger'),
-		menu = document.querySelector('.menu'),
-		menuItem = document.querySelectorAll('.menu__links-link'),
-		veil = document.querySelector('.veil'),
-		closeBtn = document.querySelector('.menu__close');
+//previous menu with veil
+// window.addEventListener('DOMContentLoaded', () => {
+// 	const hamburger = document.querySelector('.hamburger'),
+// 		menu = document.querySelector('.menu'),
+// 		menuItem = document.querySelectorAll('.menu__links-link'),
+// 		veil = document.querySelector('.veil'),
+// 		closeBtn = document.querySelector('.menu__close');
 
-	hamburger.addEventListener('click', () => {
-		menu.classList.toggle('menu-active');
-		veil.classList.toggle('presentation-veil');
+// 	hamburger.addEventListener('click', () => {
+// 		menu.classList.toggle('menu-active');
+// 		veil.classList.toggle('presentation-veil');
+// 	});
+// 	closeBtn.addEventListener('click', () => {
+// 		menu.classList.toggle('menu-active');
+// 		veil.classList.toggle('presentation-veil');
+// 	});
+// 	menuItem.forEach(item => {
+//       item.addEventListener('click', () => {
+// 			menu.classList.toggle('menu-active');
+// 			veil.classList.toggle('presentation-veil');
+//       });
+//    });
+// })
+
+//recent menu
+const humburger = document.querySelector('.humburger'),
+	menu = document.querySelector('.menu'),
+	menuItem = document.querySelectorAll('.menu__link'),
+   closeBtn = document.querySelector('.menu__close');
+
+humburger.addEventListener('click', () => {
+   menu.classList.add('active');
+});
+closeBtn.addEventListener('click', () => {
+   menu.classList.remove('active');
+});
+menuItem.forEach(item => {
+	item.addEventListener('click', () => {
+		menu.classList.remove('active');
 	});
-	closeBtn.addEventListener('click', () => {
-		menu.classList.toggle('menu-active');
-		veil.classList.toggle('presentation-veil');
-	});
-	menuItem.forEach(item => {
-      item.addEventListener('click', () => {
-			menu.classList.toggle('menu-active');
-			veil.classList.toggle('presentation-veil');
-      });
-   });
-})
+});
+
+const percent = document.querySelectorAll('.percentage__sup-percent'),
+   coloredLine = document.querySelectorAll('.percentage__sub-color');
+
+percent.forEach((item, i) => {
+   coloredLine[i].style.width = item.innerHTML;
+});
 
 //validation
 function validateForms(form) {
@@ -75,3 +100,16 @@ $("a[href='#up']").click(function () {
 	$('html, body').animate({ scrollTop: $(_href).offset().up + 'px' });
 	return false;
 });
+
+//buttons
+// const button = document.querySelectorAll('.presentation__button');
+
+// button.addEventListener('click', () => {
+//    button.classList.toggle('active');
+// });
+
+// button.forEach(item => {
+// 	item.addEventListener('click', () => {
+// 		button.classList.toggle('active');
+// 	});
+// });
